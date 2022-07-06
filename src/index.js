7c1fde8c6fdcfd5b05eb4ff331b1fb5fd703a13f
James <james@Latitude-E5450-91a33ead.localdomain>
2022-07-06 18:09:28 +0200

    add index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.scss';
+
+// const section = document.querySelector('section');
+const listContainer = document.querySelector('.todos-container');
+// const btn = document.getElementById('completed-btn');
+const myTodos = document.createElement('li');
+listContainer.appendChild(myTodos);
