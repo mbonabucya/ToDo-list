061d85cbe097ea84930648a656cb686efab98797
James <james@Latitude-E5450-91a33ead.localdomain>
2022-07-09 08:07:03 +0200

    implement add and remove functionality   all changes should be reflected in local storage

--- src/index.js
+++ src/index.js
@@ -1,85 +1,24 @@
 import './style.css';
-// const section = document.querySelector('section');
-const listContainer = document.querySelector('.todos-container');
-// const btn = document.getElementById('completed-btn');
-// const textInput = document.querySelector('input');
+import addNewTask from './addlist.js';
+import populateList from './populate.js';
+import trashCompleted from './done.js';
 
-// class Myobject {
-//   constructor(description, completed, index) {
-//     this.description = description;
-//     this.completed = completed;
-//     this.index = index;
-//   }
-// }
+const addNewTaskInput = document.querySelector('#text');
+const addNewTaskBtn = document.querySelector('.add');
+const clearCompletedTask = document.querySelector('.clear');
 
-const myTasks = [
-  {
-    index: 1,
-    description: 'task 1',
-    completed: false,
+addNewTaskBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  addNewTask(addNewTaskInput);
+});
 
-  },
-  {
-    index: 2,
-    description: 'task 2',
-    completed: false,
+clearCompletedTask.addEventListener('click', (e) => {
+  e.preventDefault();
+  trashCompleted();
+  populateList();
+});
 
-  },
-  {
-    index: 3,
-    description: 'task 3',
-    completed: false,
-
-  },
-  {
-    index: 4,
-    description: 'task 4',
-    completed: false,
-
-  },
-  {
-    index: 5,
-    description: 'task 5',
-    completed: false,
-
-  },
-  {
-    index: 6,
-    description: 'task 6',
-    completed: false,
-
-  },
-  {
-    index: 7,
-    description: 'task 7',
-    completed: false,
-
-  },
-  {
-    index: 4,
-    description: 'task 8',
-    completed: false,
-
-  },
-];
-
-const addTodo = (todoValue) => {
-  const {
-    description,
-  } = myTasks[todoValue];
-
-  const myTodos = document.createElement('div');
-  myTodos.className = 'tasks';
-  myTodos.innerHTML += `
-  <input type="checkbox" class="checkboxes">
-  <span>${description}</span>
-  <i class="fa-solid fa-ellipsis-vertical"></i>
-  <i class="fa-solid fa-trash"></i>
-
-
-  `;
-  listContainer.appendChild(myTodos);
-};
-for (let i = 0; i < myTasks.length; i += 1) {
-  addTodo(i);
-}
+document.addEventListener('DOMContentLoaded', (e) => {
+  e.preventDefault();
+  populateList();
+});
